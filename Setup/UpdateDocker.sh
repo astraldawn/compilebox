359ab7a79bc2ea9525f07b2cfadf23aa9782d9b0
Mark Docker <markleechuyong@gmail.com>
2018-01-31 01:18:40 +0000

    Updates for deployment

--- Setup/UpdateDocker.sh
+++ Setup/UpdateDocker.sh
@@ -1,5 +1,5 @@
 
 echo "Creating Docker Image"
-docker build -t 'virtual_machine' - < Dockerfile
+docker build -t virtual_machine --build-arg CACHEB=$(date +%s) .
 echo "Retrieving Installed Docker Images"
 docker images
